741c0318922226a43f42365d0fee46db6da3cbb2
tavo826 <gustavo.orozcom68@gmail.com>
2022-04-24 21:16:06 -0500

    initial commit

---
+++ src/test/java/com/wallet/wallet/WalletApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wallet.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
