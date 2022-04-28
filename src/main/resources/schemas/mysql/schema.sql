fe93b1515449a891c8450b659bd8770dad54cb55
AnthonyMerive <87150629+AnthonyMerive@users.noreply.github.com>
2022-04-28 12:00:53 -0500

    agregando schemas de postgres y mysql

---
+++ src/main/resources/schemas/mysql/schema.sql
@@ -0,0 +1,103 @@
+--
+-- Estructura de tabla para la tabla `reminders`
+--
+
+CREATE TABLE `reminders` (
+  `reminder_id` int(11) NOT NULL,
+  `wallet_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `remind` DATETIME NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `quantity` double(15,2) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL,
+  `wallet_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `quantity` double(15,2) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `wallets`
+--
+
+CREATE TABLE `wallets` (
+  `wallet_id` int(11) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  `total` double(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reminders`
+--
+ALTER TABLE `reminders`
+  ADD PRIMARY KEY (`reminder_id`),
+  ADD KEY `fk_reminders_wallets` (`wallet_id`);
+
+--
+-- Indices de la tabla `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `fk_transactions_wallets` (`wallet_id`);
+
+--
+-- Indices de la tabla `wallets`
+--
+ALTER TABLE `wallets`
+  ADD PRIMARY KEY (`wallet_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `reminders`
+--
+ALTER TABLE `reminders`
+  MODIFY `reminder_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `wallets`
+--
+ALTER TABLE `wallets`
+  MODIFY `wallet_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reminders`
+--
+ALTER TABLE `reminders`
+  ADD CONSTRAINT `fk_reminders_wallets` FOREIGN KEY (`wallet_id`) REFERENCES `wallets` (`wallet_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `fk_transactions_wallets` FOREIGN KEY (`wallet_id`) REFERENCES `wallets` (`wallet_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
